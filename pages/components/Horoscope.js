99887bd17cc094f99c43009e479e5c6f014f17d3
Austine Gomez <austine.gomez@yahoo.com>
2023-05-20 12:09:55 -0500

    refactor and image grid

--- pages/components/Horoscope.js
+++ pages/components/Horoscope.js
@@ -1,112 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
 import mapboxgl from "mapbox-gl";
 import React, { useEffect, useState, useRef } from "react";
-import ReactDOM from "react-dom";
-import geoJson from "../data/locations.json"
 import signs from "../data/horoscope.json"
 import Image from "next/image";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import { ArrowDown, ArrowUp} from 'react-bootstrap-icons';
-
-
-
+import ReactImageFallback from "react-image-fallback";
 
 const Map = () => {
+  const { features: dataForDisplay } = signs;
+  const fallback = "https://i.ibb.co/ZGLW03w/loading1.gif";
+  const [isLoading, setIsLoading] = useState(false);
   
-
-  const [title, setTitle] = useState([signs.features[0].properties.title])
-  const [range, setRange] = useState([signs.features[0].properties.range])
-  const [content, setContent] = useState([signs.features[0].properties.content])
-    const [image2,setImage] = useState([signs.features[0].properties.image2])
-  const [expanded, setExpanded] = useState(false)
-  //const dataForDisplay = expanded ? geoJson.features : geoJson.features.slice(0, 9)
-    const dataForDisplay = signs.features
-    console.log(dataForDisplay)
-
-  const [selectedLocation, setSelectedLocation] = useState({
-    name: "",
-
-  });
-
-
-  const handleSelectLocation = (signs) => {
-    const { range, content, image2,title} = signs.properties;
-    setTitle(title);
-    setRange(range);
-    setContent(content);
-    setImage(image2);
-
-
-    setSelectedLocation({
-        title: signs.properties.title,
-        range: signs.properties.range,
-        content: signs.properties.content,
-        image2: signs.properties.image2,
-    });
-
-  };  
-
   return (
-
-
     <Container>
-      <style type="text/css">
-        {`
-    .btn-flat {
-      background-color: #372545;
-      color: white;
-      margin-bottom: 1rem;
-      width:50%;
-    }
-    .btn-flat:hover {
-      background-color: white;
-      color:  #372545;
-      margin-bottom: 1rem;
-      width:50%;
-      border: 1px solid #372545;
-    }
-    .btn-xxl {
-      font-size: 1.5rem;
-    }
-    `}
-
-      </style>
-      <Row className="text-center" >
-        <div className="image-box-container">
-          {dataForDisplay.map((title, index) => (
-            <ul key={index} onClick={() => handleSelectLocation(title)}>
-  {/*           <a  href="#side">
-              <Image src={signs.features[index].properties.image} width={100} height={100} alt="location-image" className="image-list" />
-              </a> */}
-              <p>{signs.features[index].properties.title}</p>
-            </ul>
+      {isLoading ? (
+        <img src={fallback} alt="loading" />
+      ) : (
+        <Row className="text-center">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Col sm key={index}>
+              <div className="image-box-container">
+                {dataForDisplay.slice(index * 4, index * 4 + 4).map((sign, subIndex) => (
+                  <ul key={subIndex}>
+                    <a href={`/sign/${sign.properties.title}`}>
+                      <p>{sign.properties.title}</p>
+                      <ReactImageFallback
+                        src={sign.properties.image}
+                        fallbackImage={fallback}
+                        initialImage={fallback}
+                        alt="signimage"
+                        className="image-list"
+                      />
+                    </a>
+                  </ul>
+                ))}
+              </div>
+            </Col>
           ))}
-
-        </div>
-
-        <Col sm={4}>
-          <div className="sidebar" id="side">
-            <a href={`/sign/${title}`} >
-            <h1> {title} </h1>
-            </a>
-            <p> {title} </p>
-            <p> {range} </p>
-            <p>{content} </p>
-          </div>
-
-        </Col>
-        <Col sm={4}>
-        <a >
-              <img src={image2} className="display-image" alt="featured-image" width={400} height={400} />
-            </a>
-        </Col>
-      </Row>
+        </Row>
+      )}
     </Container>
-  )
+  );
 };
+
 
 
 export default Map;
--- pages/sign/[slug].js
+++ pages/sign/[slug].js
@@ -5,29 +5,19 @@ import Navbar from '../components/Navbar'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {VictoryPie, VictoryLabel} from 'victory';
+import { VictoryPie, VictoryLabel } from 'victory';
 import { useEffect, useState } from 'react';
 
 
-
-
-
 const Page = ({ title, range, content, image, compatabilitySign1, compatabilitySign2, compImage1, compImage2 }) => {
-    const router = useRouter()
-    const { id } = router.query
-    const percent  = 90; // Calculating the percentage
-    const remainingPercentage = 100 - percent; // Calculating the remaining percentage
-
-    const graphicColor = ['#388087', '#6fb3b8', '#badfe7']; // Colors
-    const wantedGraphicData = [{ y: 0 }, { y: percent }, { y: remainingPercentage }]; // Data that we want to display
-    const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }]; // Data used to make the animate prop work
+    const router = useRouter();
+    const [graphicData, setGraphicData] = useState([{ y: 0 }, { y: 0 }, { y: 100 }]); // Data used to make the animate prop work
     
-
-
-    const [graphicData, setGraphicData] = useState(defaultGraphicData);
-
     useEffect(() => {
-      setGraphicData(wantedGraphicData); // Setting the data that we want to display
+        const percent = 90;
+        const remainingPercentage = 100 - percent;
+        const wantedGraphicData = [{ y: 0 }, { y: percent }, { y: remainingPercentage }];
+        setGraphicData(wantedGraphicData); 
     }, []);
 
     return (
@@ -36,74 +26,57 @@ const Page = ({ title, range, content, image, compatabilitySign1, compatabilityS
             <Container>
                 <Row>
                     <Col md={8}>
-                        <h1> {title}</h1>
+                        <h1>{title}</h1>
                     </Col>
-
                     <Col sm={4}>
                         <div className="image-box">
                             <Image src={image} width={300} height={300} alt="image" className='bio-image' />
-                        </div> 
+                        </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                 {range}
-                <p> {content}</p>
-                 <p>{compatabilitySign1}</p>
-                 <text x={100} y={110} textAnchor="middle" >
-                        {percent}%
-                        </text>
-                 <Image src={compImage1} width={100} height={100} alt="image" className='bio-image' />  
-                 <p>{compatabilitySign2}</p>
-                 <Image src={compImage2} width={100} height={100} alt="image" className='bio-image' />
+                        <p>{range}</p>
+                        <p>{content}</p>
                     </Col>
                 </Row>
-                <svg viewBox="0 0 900 900" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" className="svg-container" style={{ width: "50%", height: "50%" }}>
-                <VictoryPie standalone={false} data={graphicData} width={250} height={250} colorScale={graphicColor}  innerRadius={68} labelRadius={100} animate={{ easing: 'exp' }} style={{ labels: { fontSize: 20, fill: "white" } }}/>
-                 <VictoryLabel textAnchor="middle" style={{ fontSize: 20 }} x={125} y={125} text={percent + "%"} />
-                </svg>
-                <svg viewBox="0 0 900 900" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" className="svg-container" style={{ width: "50%", height: "50%" }}>               
-                 <VictoryPie standalone={false} data={graphicData} width={250} height={250} colorScale={graphicColor}  innerRadius={68} labelRadius={100} animate={{ easing: 'exp' }} style={{ labels: { fontSize: 20, fill: "white" } }}/>
-                 <VictoryLabel textAnchor="middle" style={{ fontSize: 20 }} x={125} y={125} text={percent + "%"} />
-                </svg>
-            </Container>
+                <div className="chart-container">
+                    <p>{compatabilitySign1}</p>
+                    <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" className="svg-container" style={{ width: "400px", height: "400px" }}>
+                        <VictoryPie standalone={false} data={graphicData} width={250} height={250} colorScale={['#388087', '#6fb3b8', '#badfe7']} innerRadius={68} labelRadius={100} animate={{ easing: 'exp' }} style={{ labels: { fontSize: 20, fill: "white" } }} />
+                        <VictoryLabel textAnchor="middle" style={{ fontSize: 20 }} x={125} y={125} text="90%" />
+                    </svg>
+                    <p>{compatabilitySign2}</p>
 
+                    <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" className="svg-container" style={{ width: "400px", height: "400px" }}>
+                        <VictoryPie standalone={false} data={graphicData} width={250} height={250} colorScale={['#388087', '#6fb3b8', '#badfe7']} innerRadius={68} labelRadius={100} animate={{ easing: 'exp' }} style={{ labels: { fontSize: 20, fill: "white" } }} />
+                        <VictoryLabel textAnchor="middle" style={{ fontSize: 20 }} x={125} y={125} text="90%" />
+                    </svg>
+                </div>
+            </Container>
         </div>
     )
-}
+} 
+
+
 export async function getStaticPaths() {
     const categories = signs.features;
-    const paths = categories.map(({ properties: { title, range, content,image, compatabilitySign1, compatabilitySign2, compImage1, compImage2 } }) => ({
-        params: { slug: title, range, content, image, compatabilitySign1, compatabilitySign2,  compImage1, compImage2}
-    }));
+    const paths = categories.map(({ properties: { title } }) => ({ params: { slug: title } }));
+  
+    return { paths, fallback: false };
+  }
+  
 
-    return {
-        paths,
-        fallback: false
-    };
-}
-
-export async function getStaticProps({ params }) {
+  export async function getStaticProps({ params }) {
     const categories = signs.features;
     const category = categories.find(
         ({ properties: { title } }) => title === params.slug
     );
-
-    return {
-        props: {
-            title: category.properties.title,
-            range: category.properties.range,
-            content: category.properties.content,
-            image: category.properties.image ,
-            compatabilitySign2: category.properties.compatabilitySign2,
-            compatabilitySign1: category.properties.compatabilitySign1,
-            compImage1: category.properties.compImage1,
-            compImage2: category.properties.compImage2,
-
-        }
-    };
-}
-
+  
+    const { title, range, content, image, compatabilitySign1, compatabilitySign2, compImage1, compImage2 } = category.properties;
+    
+    return { props: { title, range, content, image, compatabilitySign2, compatabilitySign1, compImage1, compImage2 } }
+  }
 
 
 
