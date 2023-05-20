15dbf891834c52868c834875fa0cde2fd320cb6f
Austine Gomez <austine.gomez@yahoo.com>
2023-05-20 16:44:26 -0500

    home grid

--- pages/components/Horoscope.js
+++ pages/components/Horoscope.js
@@ -9,42 +9,39 @@ import Col from 'react-bootstrap/Col';
 import ReactImageFallback from "react-image-fallback";
 
 const Map = () => {
-  const { features: dataForDisplay } = signs;
-  const fallback = "https://i.ibb.co/ZGLW03w/loading1.gif";
-  const [isLoading, setIsLoading] = useState(false);
-  
+  const dataForDisplay = signs.features
+  const [isLoading, setIsLoading] = useState(false)
+  if (isLoading) {
+    return <img src="https://i.ibb.co/ZGLW03w/loading1.gif" alt="loading" />
+  }
+  const fallback = "https://i.ibb.co/ZGLW03w/loading1.gif"
   return (
     <Container>
-      {isLoading ? (
-        <img src={fallback} alt="loading" />
-      ) : (
-        <Row className="text-center">
-          {Array.from({ length: 3 }).map((_, index) => (
-            <Col sm key={index}>
-              <div className="image-box-container">
-                {dataForDisplay.slice(index * 4, index * 4 + 4).map((sign, subIndex) => (
-                  <ul key={subIndex}>
-                    <a href={`/sign/${sign.properties.title}`}>
-                      <p>{sign.properties.title}</p>
-                      <ReactImageFallback
-                        src={sign.properties.image}
-                        fallbackImage={fallback}
-                        initialImage={fallback}
-                        alt="signimage"
-                        className="image-list"
-                      />
-                    </a>
-                  </ul>
-                ))}
-              </div>
-            </Col>
-          ))}
-        </Row>
-      )}
+      <Row className="text-center">
+        {Array.from({ length: 3 }, (_, index) => (
+          <Col sm key={index}>
+            <div className="image-box-container">
+              {dataForDisplay.slice(index * 4, index * 4 + 4).map((title, subIndex) => (
+                <ul key={subIndex} className="ul-img">
+                  <a href={`/sign/${signs.features[subIndex + index * 4].properties.title}`}>
+                    <p>{signs.features[subIndex + index * 4].properties.title}</p>
+                    <ReactImageFallback
+                      src={signs.features[subIndex + index * 4].properties.image}
+                      fallbackImage={fallback}
+                      initialImage="https://i.ibb.co/ZGLW03w/loading1.gif"
+                      alt="signimage"
+                      className="image-list"
+                    />
+                  </a>
+                </ul>
+              ))}
+            </div>
+          </Col>
+        ))}
+      </Row>
     </Container>
-  );
+  )
 };
-
 
 
 export default Map;
